6ed279e3a9d9034c2379bc586185d31342f996f4
Yuri <fryssss@yandex.ru>
2019-03-08 15:32:51 +0300

    add css&js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("trigger").onclick = function() {
+  open();
+};
+
+function open() {
+  document.getElementById("menu").classList.toggle("show");
+}
+
+// function func() {
+//   ;
+// }
+
+// setTimeout(func, 3000);
